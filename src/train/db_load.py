0b44a65a75e74013b25ecb494cfe1d727990e671
pawel-rozwoda <pawel202@protonmail.com>
2022-11-06 15:48:44 +0100

    new db load approach

--- src/train/db_load.py
+++ src/train/db_load.py
@@ -6,65 +6,8 @@ from torch import randint
 import psycopg2
 from db_credentials import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_HOST
 import sys 
-
-class MFCC_Dataset(Dataset):
-    def __init__(self, *, db_name, fixed_area_sample=False, feats_times_ten=10): 
-        
-        if feats_times_ten <= 0:
-            sys.exit('feats_times_ten must be >= 0')
-
-        self.chunks_count=feats_times_ten
-
-        self.db_name=db_name
-        conn = psycopg2.connect(dbname=db_name, user=POSTGRES_USER, password=POSTGRES_PASSWORD, host=POSTGRES_HOST, port=POSTGRES_PORT)#, sslmode='require')
-        cur = conn.cursor()
-
-        cur.execute("select table_name from information_schema.tables where table_name like 'mfcc%';")
-        self.table_names = [i[0] for i in cur.fetchall()]
-        self.lengths = []
-        for t_name in self.table_names:
-            cur.execute('select count(*) from ' + t_name)
-            r_count = cur.fetchone()[0]
-            self.lengths.append(r_count // self.chunks_count)
-
-        cur.close()
-        conn.close() 
-        # print(self.table_names, end='\n\n')
-        # print(self.lengths)
-
-
-        self.fixed_area_sample=fixed_area_sample 
-        
-    
-    def __len__(self):
-        return len(self.table_names)
-
-
-    def __getitem__(self, idx):
-        conn = psycopg2.connect(dbname=self.db_name, user=POSTGRES_USER, password=POSTGRES_PASSWORD, host='localhost', port=POSTGRES_PORT)
-        cur = conn.cursor()
-
-        if self.fixed_area_sample:
-            cur.execute('select feats from ' + self.table_names[idx] + ' where ID Between 0 and ' + str( self.chunks_count ) )
-
-        else:
-            r_idx = randint(1, self.lengths[idx], (1,)).item()
-            cur.execute('select feats from ' + self.table_names[idx] + ' where ID Between ' + str(r_idx) + ' and ' + str(r_idx + self.chunks_count -1) )
-        x = None
-        try:
-            x = cur.fetchall()
-            
-        except Exception as e:
-            print('lengths ', self.lengths[idx])
-            print('idx ', idx)
-            print('row idx ', r_idx)
-            print(type(x))
-
-        res = torch.Tensor(x).reshape(-1, 40)
-        cur.close()
-        conn.close()
-        
-        return res
+# from get_batch import Get_Batch
+sys.path.append('../')
 
 
 def dataset_split(*, dataset, train_partition):
@@ -76,3 +19,105 @@ def dataset_split(*, dataset, train_partition):
     train_dataset, validation_dataset = torch.utils.data.random_split(dataset, [train_size, validation_size]) 
 
     return train_dataset, validation_dataset
+
+class MFCC_Dataset(Dataset):
+    def sorted_length_speakers(self):
+        """
+        returns list of speaker_ids in descending speech length like: 
+        ['mfcc_5', 'mfcc_47', 'mfcc_12', ... etc]
+
+        lengths = [4845, 4756, 4512, 4205, 4123, ... ] 
+        batch_n = [0,0,0,0,0,1,1,1,1,2,2,3]
+        """
+
+
+        self.cursor.execute("SELECT table_name FROM information_schema.tables \
+                WHERE table_name like 'mfcc_%' ORDER BY table_name;")
+        speaker_ids=[i[0] for i in self.cursor.fetchall()]
+        self.lengths=dict()
+        for speaker in speaker_ids:
+            self.cursor.execute("SELECT count(*) from " + speaker)
+            speaker_length = self.cursor.fetchone()[0]
+            self.lengths[speaker] = speaker_length
+        aux = [i[0] for i in sorted(self.lengths.items(), key=lambda item: item[1], reverse=True)]
+        return list(aux)
+
+    def prepare_batches(self, sorted_speakers):
+        s=0
+        for i in range(len(sorted_speakers)//self.batch_size):
+            bucket_speakers = sorted_speakers[i*self.batch_size : (i+1)*self.batch_size]
+            self.bucket.append( bucket_speakers )
+            last = self.bucket[-1][-1]
+            self.bucket_bottom.append(s)
+            last_idx = self.lengths[last] // self.occ_len
+            s += last_idx
+            # print(f"init -> bucket: {bucket_speakers}")
+            # print(f"last: {last}, len = {last_idx} ")
+            # print()
+
+            for j in range(last_idx):
+                self.batch_n.append(i)
+
+        self.batch_count = len(self.batch_n)
+        pass
+
+    def _get_batch(self, idx):
+        return self.batch_n[idx]
+
+    def _get_bottom(self, idx):
+        return self.bucket_bottom[self.batch_n[idx]]
+
+    def __init__(self, db_name, batch_size, occ_len):
+        """
+        bucket: [
+                 [mfcc_56, mfcc_41, mfcc_77, mfcc_21] # four speakers with longest speech
+                 [ ...                              ]
+                ]
+        """
+
+        con = psycopg2.connect(database=db_name, user=POSTGRES_USER, password=POSTGRES_PASSWORD, host=POSTGRES_HOST, port=POSTGRES_PORT) 
+
+        self.batch_size = batch_size
+        self.occ_len = occ_len
+        self.con = con
+        self.cursor = con.cursor()
+        self.batch_count = 0
+        self.bucket = []
+        self.bucket_bottom = []
+        self.batch_n = []
+
+        sorted_speakers = self.sorted_length_speakers()
+        self.prepare_batches(sorted_speakers)
+        print(f"bottoms: {self.bucket_bottom}")
+
+
+    def __getitem__(self, idx):
+        if idx<0:
+            idx = self.batch_count + idx
+        b = self.bucket[self.batch_n[idx]]
+        res = []
+        bottom = self._get_bottom(idx)
+        idx_from = str(1 + (idx*self.occ_len) - (bottom*self.occ_len))
+        idx_to = str((idx*self.occ_len) + self.occ_len - (bottom*self.occ_len))
+        # previous_bucket_len = sum(self.bucket_bottom(i) for i in range(self.batch_n[idx]))
+        # print(f"previous_len: {previous_bucket_len}")
+        # print(f"buckets: {self.bucket_bottom}")
+        # print(f'DEBUG: bucket  {b}')
+        # print(f"bottom: {bottom}")
+                                
+        # print(f"from: {idx_from}; to: {idx_to}")
+
+        for i in b:
+            # print(f'selected table:{i}')
+            self.cursor.execute('select feats from ' + i + ' where ID Between ' + idx_from + ' and ' + idx_to)
+
+            res.append(self.cursor.fetchall())
+        r = torch.Tensor(res).reshape(self.batch_size, self.occ_len, -1)
+        return r
+
+    def __len__(self):
+        return self.batch_count
+
+    def __del__(self):
+        print("closing connection to db")
+        self.con.close()
--- src/train/train.py
+++ src/train/train.py
@@ -1,8 +1,9 @@
+
 import sys
 sys.path.append('../')
 from model import LSTM_Diarization
 import torch
-from db_load import MFCC_Dataset, dataset_split
+from db_load import MFCC_Dataset, dataset_split, MFCC_Dataset
 from tqdm import tqdm, trange
 from config import DATA_PATH, OUT_TRAIN
 from aux import *
@@ -14,11 +15,12 @@ import csv
 import torch.utils.data as data_utils
 from datetime import datetime
 from torch.utils.data import DataLoader, RandomSampler
+from tqdm import tqdm
 
 torch.manual_seed(2)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--epochs", type=int, default=1)
+parser.add_argument("--epochs", type=int, default=1000)
 parser.add_argument("--num_workers", type=int)
 parser.add_argument("--dataset", type=str)
 parser.add_argument("--optional_validation", type=str, default=None)
@@ -29,6 +31,10 @@ args = parser.parse_args()
 print('pid ', os.getpid())
 print('initial w ', args.init_w)
 print('initial b ', args.init_b)
+
+input_dim = 40
+hidden_dim = 768
+num_layers = 3 
 
 DB_NAME = args.dataset
 OPTIONAL_VALIDATION_DB_NAME = args.optional_validation
@@ -44,180 +50,91 @@ if not os.path.exists(out_dir):
 print('warm restart ', args.warm_restart)
 print('num_workers: ', args.num_workers) 
 
-
-input_dim = 40
-hidden_dim = 768
-num_layers = 3 
-batch_size = 16
+train_dataset = MFCC_Dataset(db_name='vox_1_test', batch_size=8, occ_len=60)
+split_index = 82
+print(f"len: {len(train_dataset)}")
 
 
-num_workers=args.num_workers
-
-
-""" split dataset to train and test """
-train_partition = 0.9 
-
-# full_dataset = MFCC_Dataset(db_name=DB_NAME, feats_times_ten=40, fixed_area_sample=True)
-full_dataset = MFCC_Dataset(db_name=DB_NAME, feats_times_ten=40)
-train_dataset, validation_dataset = dataset_split(dataset=full_dataset, train_partition=train_partition)
-""" end split """
-
-print('train split: ', train_partition)
-print('train_size', len(train_dataset))
-print('validation size', len(validation_dataset))
-
-
-# train_loader = DataLoader( train_dataset, batch_size=batch_size, sampler=RandomSampler(train_dataset), num_workers=num_workers)
-train_loader = DataLoader( train_dataset, batch_size=batch_size, num_workers=num_workers)
-validation_loader = DataLoader( validation_dataset, batch_size=batch_size, num_workers=num_workers)
-
-if len(validation_dataset) % batch_size == 1 or len(train_dataset) % batch_size == 1:
-    sys.exit('batch size cannot be 1 in any iteration') 
-
-if args.optional_validation:
-    optional_validation_dataset = MFCC_Dataset(db_name=OPTIONAL_VALIDATION_DB_NAME, fixed_area_sample=True)
-    optional_validation_loader = DataLoader( optional_validation_dataset, batch_size=batch_size, num_workers=num_workers)
-
-
-
-""" reduced """
-reduced_size = len(validation_dataset)
-reduced_train_dataset = data_utils.Subset(train_dataset, torch.arange(0,reduced_size ))
-reduced_train_loader = torch.utils.data.DataLoader(reduced_train_dataset, batch_size=batch_size, shuffle=True) 
-""" end reduced """
-
-
-print('train: ' + DB_NAME + ': ' + str(len(train_dataset)))
-print('train reduced ', str(len(reduced_train_dataset)))
-print('optional validation ' + str(OPTIONAL_VALIDATION_DB_NAME))
+model = LSTM_Diarization(input_dim=input_dim, hidden_dim=hidden_dim, num_layers=num_layers, init_w=10., init_b=-5.)
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print('Using device:', device)
-
-model = LSTM_Diarization(input_dim=input_dim, hidden_dim=hidden_dim, num_layers=num_layers, init_w=args.init_w, init_b=args.init_b)
+model = model.to(device)
 model.train=True
+most_recent_validation = 10.
+criterion = CELoss(device)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+print('Using device:', device)
 
 with open(out_dir + 'info.txt', "w") as myfile:
     myfile.write('param w: ' + str(model.w) + '\n' ) 
     myfile.write('param b: ' + str(model.b) + '\n' ) 
     myfile.write('warm restarts ' +  str(args.warm_restart) + '\nepochs ' + str(args.epochs) + '\n') 
     myfile.write('train_dataset ' +  str(args.dataset) + '\n') 
-    myfile.write('optional validation ' +  str(args.optional_validation) + '\n') 
-
-print('model dtype', model.dtype)
-model.to(device) 
-model.train = True
-
-scheduler = None
-lr = 0.01
-optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 
 if args.warm_restart:
     T_0 = 40
     T_mult = 2
     scheduler = CosineAnnealingWarmRestarts(optimizer, T_0, T_mult)
+    iters = len(train_dataset)
+    current_iter = 0
 
-epochs = args.epochs
-print(epochs, ' epochs')
 
-iters = len(train_loader)
+for epoch in tqdm(range(args.epochs)):
 
-most_recent_validation = 10.
-criterion = CELoss(device)
-for epoch in trange(epochs):
-    if not args.warm_restart:
-        if  (epoch % 300) + 1 == 0:
-            optimizer.param_groups[0]['lr'] /= 2
-            # print('lr change:', scheduler.get_last_lr())
-
-    """ train and validation are subsets of full_dataset """
-    full_dataset.fixed_area_sample=False
-    """ """
-    grads_stacked = []
-    for i, batch in enumerate(train_loader):
-        """batch or "feats" shape: i.e (64, 10, 40)"""
-
+    for j in tqdm(range(split_index)):
+        batch = train_dataset[j]
         batch = batch.to(device) 
-        batch = batch.squeeze(0)
-        
+        # batch = batch.squeeze(0)
         pred = model(batch)
 
         loss = criterion(pred) 
-
         optimizer.zero_grad()
         loss.backward()
+        optimizer.step()
+
+        if args.warm_restart:
+            scheduler.step((epoch + current_iter) / iters)
+
         # clipping_value = 3 # arbitrary value of your choosing
         # torch.nn.utils.clip_grad_norm(model.parameters(), clipping_value)
 
-        """ for analysis purposes """
-        gradients_lengths = []
-        for w in model.parameters():
-            if w.grad is not None:
-                n = torch.norm(w.grad)
-                gradients_lengths.append(n) 
-        gradients_lengths = torch.Tensor(gradients_lengths) 
-        grads_stacked.append(gradients_lengths)
-        """ end """
-
-
-        optimizer.step() 
-
-        if args.warm_restart:
-            scheduler.step(epoch + i / iters)
-
-
-    if epoch==epochs-1:
+    if epoch==args.epochs-1:
         print('saving example pred in epoch = ', epoch)
         torch.save(pred.cpu(), out_dir + 's_matrix.pt')
 
-    st = torch.stack(grads_stacked)
-    st = st.mean(axis=0)
-
-    """ train and validation are subsets of full_dataset """
-    full_dataset.fixed_area_sample=True
-    """ """
     train_losses = []
+    validation_losses = []
     with torch.no_grad():
-        for batch in reduced_train_loader:
+        print("train loss")
+        for i in range(split_index//4):
+            batch = train_dataset[i]
             batch = batch.to(device).squeeze(0)
             pred = model(batch)
-
             loss = criterion(pred)
             train_losses.append(loss.item()) 
 
-    validation_losses=[]
-    with torch.no_grad():
-        for batch in validation_loader:
-            batch = batch.to(device).squeeze(0) 
+        print("validation loss")
+        for i in range(split_index, len(train_dataset)):
+            batch = train_dataset[i]
+            batch = batch.to(device).squeeze(0)
             pred = model(batch)
+            loss = criterion(pred)
+            validation_losses.append(loss.item()) 
 
-            loss = criterion(pred) 
-            validation_losses.append(loss.item())
-
-    if args.optional_validation:
-        optional_validation_losses=[]
-        with torch.no_grad():
-            for batch in optional_validation_loader:
-                batch = batch.to(device).squeeze(0) 
-                pred = model(batch)
-
-                loss = criterion(pred) 
-                optional_validation_losses.append(loss.item())
-
-        with open(out_dir + 'optional_validation_losses.csv', "a") as myfile:
-            myfile.write(','.join(map(str, (epoch, np.mean(optional_validation_losses)))) + '\n')
-
-
-
+    if args.warm_restart:
+        current_iter +=1
 
     with open(out_dir + 'train_losses.csv', "a") as myfile:
         myfile.write(','.join(map(str, (epoch, np.mean(train_losses)))) + '\n')
+    with open(out_dir + 'validation_losses.csv', "a") as myfile:
+        myfile.write(','.join(map(str, (epoch, np.mean(validation_losses)))) + '\n')
 
-    with open(out_dir + "validation_losses.csv", "a") as myfile:
-        myfile.write(','.join(map(str, (epoch, np.mean(validation_losses)))) + '\n') 
-
-    with open(out_dir + 'grads.txt', 'a') as myfile:
-        myfile.write( str(epoch) + ',' + ','.join(map(str, (st.flatten().tolist()))  ) + '\n')
+    if np.mean(validation_losses) < most_recent_validation:
+        most_recent_validation = np.mean(validation_losses)
+        print(f"current best model in epoch {epoch}")
+        torch.save(model, out_dir + 'best_fit_model.pt')
+        with open(out_dir + 'best_fit_update.txt', 'a') as myfile:
+            myfile.write( str(epoch) + '\n')
 
     with open(out_dir + 'lrs.csv', "a") as myfile:
         last_lr = 0
@@ -227,14 +144,3 @@ for epoch in trange(epochs):
             last_lr = optimizer.param_groups[0]['lr']
 
         myfile.write(str(epoch) + ',' + str(last_lr) + '\n')
-
-    if np.mean(validation_losses) < most_recent_validation:
-        most_recent_validation = np.mean(validation_losses)
-        torch.save(model, out_dir + 'best_fit_model.pt')
-        with open(out_dir + 'best_fit_update.txt', 'a') as myfile:
-            myfile.write( str(epoch) + '\n')
-
-
-
-
-torch.save(model, out_dir + 'last_epoch_model.pt')
